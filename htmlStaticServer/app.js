181f8769a5017cdd0aa822deaa3e2e93e71c6dcd
wangwenlin <wangwenlin@innotechx.com>
2021-04-14 17:29:27 +0800

    添加 初始化文件

--- htmlStaticServer/app.js
+++ htmlStaticServer/app.js
@@ -5,7 +5,7 @@ app.use(express.static("/wwl-games/game-test/"));
 app.get("/", function (req, res) {
 	res.send("Hello World!");
 });
-var server = app.listen(8081, function () {
+var server = app.listen(7001, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
